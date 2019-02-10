1d8abccbd5fe89ebe5b29366745c7311896a2d3e
hellosaraann <srichardson@mcad.edu>
2019-02-10 15:34:50 -0600

    Added javascript

---
+++ main.js
@@ -0,0 +1,24 @@
+
+document.getElementById('alabama').onmouseover = function() {
+  alert("Montgomery");
+};
+
+document.getElementById('alaska').onmouseover = function() {
+  alert("Juneau");
+};
+
+document.getElementById('arizona').onmouseover = function() {
+  alert("Phoenix");
+};
+
+document.getElementById('arkansas').onmouseover = function() {
+  alert("Little Rock");
+};
+
+document.getElementById('california').onmouseover = function() {
+  alert("Sacramento");
+};
+
+document.getElementById('colorado').onmouseover = function() {
+  alert("Denver");
+};
